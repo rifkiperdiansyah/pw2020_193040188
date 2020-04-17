6f15bee4aa4ee29f021c2fb47e71024e4e55dd2f
rifkiperdiansyah <63568921+rifkiperdiansyah@users.noreply.github.com>
2020-04-17 22:23:08 +0700

    css

--- praktikum/P5_PW_193040188/latihan5c/php/detail.php
+++ praktikum/P5_PW_193040188/latihan5c/php/detail.php
@@ -18,11 +18,13 @@ $judul_buku = query ("SELECT *FROM judul_buku WHERE id = $id" )[0];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Detail</title>
+    <link rel="stylesheet" href="../css/style.css">
+
 </head>
 <body>
     <div class="container">
     <img src="../asset/img/<?= $judul_buku['cover_buku']; ?>" alt="">
-    </div>
+   
 
     <div class="keterangan">
 
@@ -32,7 +34,8 @@ $judul_buku = query ("SELECT *FROM judul_buku WHERE id = $id" )[0];
     <p><?= $judul_buku['Tebal_buku']; ?></p>
   
     </div>
-
+    
     <button class="kembali"><a href=" ../index.php">back</a> </button>
+    </div>
 </body>
 </html>
