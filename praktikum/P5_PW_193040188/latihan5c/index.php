4df5e21557e8914bdab43e9bbe53ca8cd0d26524
rifkiperdiansyah <63568921+rifkiperdiansyah@users.noreply.github.com>
2020-04-18 08:33:44 +0700

    ubah

--- praktikum/P5_PW_193040188/latihan5c/index.php
+++ praktikum/P5_PW_193040188/latihan5c/index.php
@@ -18,6 +18,8 @@ $judul_buku = query("SELECT *FROM judul_buku");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="../css/style2.css">
     <title>Daftar mahasiswa</title>
 </head>
 <body>
